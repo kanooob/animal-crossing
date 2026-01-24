c618648c685456e5e270024becf298b24e9187c7
Galxie_S9 <100624075+kanooob@users.noreply.github.com>
2026-01-24 22:15:22 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -587,7 +587,7 @@ const S4D_WEBSITECREATION_EXPRESS_app = S4D_WEBSITECREATION_EXPRESS();
         <html lang="fr">
         <head>
             <meta charset="UTF-8">
-        <meta property="og:image" content="https://raw.githubusercontent.com/kanooob/animal-crossing/refs/heads/main/image.png">
+            <link rel="icon" type="image/png" href="https://raw.githubusercontent.com/kanooob/animal-crossing/refs/heads/main/image.png">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <title>Temps de fonctionnement du bot</title>
             <style>
